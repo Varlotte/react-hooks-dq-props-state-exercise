937a59f52430477f9a43a11469d19cd3be063a06
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-08-23 12:03:20 -0400

    updates if-else to make sure to use arrow syntax for best practice

--- src/components/BabyHog.js
+++ src/components/BabyHog.js
@@ -11,9 +11,9 @@ function BabyHog(props) {
     // how can we reuse this for both buttons?
     // perhaps something with e.target.name === "+"
     if (e.target.name === "+") {
-      setWeight(weight + 1);
+      setWeight((weight) => weight + 1);
     } else if (e.target.name === "-") {
-      setWeight(weight - 1);
+      setWeight((weight) => weight - 1);
     }
   }
 
